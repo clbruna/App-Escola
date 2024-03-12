332b1b22c7c82e87e04ccb6785c7fdf90d417d3f
Rogério Sobral Ribeiro <sobralcomix@gmail.com>
2024-03-12 09:19:47 -0300

    Rogério  v.1
    Realizando funções para testar a classe Professor, não esta implementando a pytest ainda.

--- Model/usuarios/professor_test.py
+++ Model/usuarios/professor_test.py
@@ -1,34 +1,73 @@
 from Model.usuarios.professor import Professor
 from faker import Faker
 import random
-
+import re
 
 def geradorSenhas():
     n1= str(random.randint(0,9))
     n2 = str(random.randint(0, 9))
     n3 = str(random.randint(0, 9))
     n4 = str(random.randint(0, 9))
-    return f"{n1}{n2}{n3}{n4}"
+    n5 = str(random.randint(0, 9))
+    return f"{n1}{n2}{n3}{n4}{n5}"
 
 
 names=Faker()
 
 constanteNiveis=["COMUM","ADM", "GERENTE"]
 constanteContrato=["CLT","ESTAGIO", "EVENTUAL"]
-print(names.name())
+
+def testarNivel(nivel):
+
+    if nivel in constanteNiveis:
+        return True
+    else:
+        return False
+
+def testaNome(nome:str) ->bool:
+    """
+    realiza um teste no campo nome
+    >> nome== "José"
+
+    :param nome:
+    :return: bool
+    """
+    regex_nome = r'^[a-zA-ZÀ-ÿ\'\s]+$'
+    if re.match(regex_nome, nome):
+        return True
+    else:
+        return False
+
+
+def testarSenha(senha:str)-> bool:
+
+    regex_senha= r'^.*\d{4}.*$'
+    if re.match(regex_senha,senha):
+        return True
+    else:
+        return False
+
+def testarContrato(contrato:str)->bool:
+    if contrato in constanteContrato:
+        return True
+    else:
+        return False
+
 
 for i in range(100):
-    id = random.randint(0, 9)
+    id = random.randint(0, 100)
     senha = geradorSenhas()
+    contrato= random.choice(constanteContrato)
+    niveis=random.choice(constanteNiveis)
 
     cons_Niv=[e for e in constanteNiveis]
-    p1=Professor(str(id),names.name(),senha,"COMUM", "CLT")
-
-    print(f"teste ID: {p1.getId.isdecimal()}")
-    print(f"teste nome:  {isinstance(p1.getNome,str)}")
-    print(f"teste senha: {p1.getSenha.isdecimal()}")
-    print(f"teste nivel: {p1.getNivel.isalpha()}")
-    print(f"teste contrato: {p1.contrato.isalpha()}")
+    p1=Professor(str(id),names.name(),senha,niveis, contrato)
+    print(f"Teste {i}")
+    print(f"teste ID: {p1.getId.isdecimal()}, id testado {id}")
+    print(f"teste nome:  {testaNome(p1.getNome)}, nome testado {p1.getNome} ")
+    print(f"teste senha: {testarSenha(p1.getSenha)}, senha testado {p1.getSenha}   ")
+    print(f"teste nivel: {testarNivel(p1.getNivel)},  nivel testado {p1.getNivel}")
+    print(f"teste contrato: {testarContrato(p1.contrato)}, nivel contrato {p1.contrato}  ")
     print("#"*40)
 
 
